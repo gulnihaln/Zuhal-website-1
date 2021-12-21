224b867dfda7ea465171700d74ce475a6ec31247
seyedsharifi12 <seyedsharifi12@gmail.com>
2021-12-21 23:55:12 +0000

    new pomponent created

--- zuhal-website/client/src/components/Navigation.js
+++ zuhal-website/client/src/components/Navigation.js
@@ -1,3 +1,43 @@
-export default function DailyHairAndMakeup() {
-  return;
+import { GoChevronDown } from "react-icons/go";
+export default function Navigation() {
+  return (
+    <>
+      <div class="navbar">
+        <a href="home">Home</a>
+        <a href="/AboutMe">About Me</a>
+        <a href="/Services">
+          Services <GoChevronDown />
+        </a>
+        <a href="/Contact">Contact</a>
+        <a href="/POrtfolio">Portfolio</a>
+        {/* <div class="dropdown">
+          <select name="cars" id="cars">
+            <optgroup label="Services">
+              <option value="/DayHairAndMakeup">Day Hair and Makeup</option>
+              <option value="/EveningHairAndMakeup">
+                Evening Hair and Makeup
+              </option>
+              <option value="/BrowThreadingAndTinting">
+                Brow Threading and Tinting
+              </option>
+            </optgroup>
+          </select> */}
+
+        {/* <div class="dropdown-content"></div>
+        </div> */}
+      </div>
+      <div class="dropdown">
+        <button class="dropbtn">
+          Dropdown
+          <i class="fa fa-caret-down"></i>
+        </button>
+
+        <div class="dropdown-content">
+          <a href="/link1">Link 1</a>
+          <a href="/link2">Link 2</a>
+          <a href="/link3">Link 3</a>
+        </div>
+      </div>
+    </>
+  );
 }
