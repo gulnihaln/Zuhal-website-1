81b3304e3c312807102f8fe5005f0409ee17e26c
Sharm-Dev <sharmainetaylor@ymail.com>
2021-12-21 09:31:30 +0000

    Added initial content for Royalty and Gift Card component fixes #11

--- zuhal-website/client/src/components/RoyaltyAndGiftCard.js
+++ zuhal-website/client/src/components/RoyaltyAndGiftCard.js
@@ -1,3 +1,24 @@
 export default function RoyaltyAndGiftCard() {
-  return;
+  return (
+    <>
+      <section>
+        <img scr="https://via.placeholder.com/640x332.png" alt="Placeholder" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          debitis incidunt sequi quisquam cum fuga rem ipsum aperiam nisi
+          accusamus natus magni placeat quis eaque libero dolore ab dignissimos
+          ea in, fugiat delectus.
+        </p>
+      </section>
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          debitis incidunt sequi quisquam cum fuga rem ipsum aperiam nisi
+          accusamus natus magni placeat quis eaque libero dolore ab dignissimos
+          ea in, fugiat delectus.
+        </p>
+        <img scr="https://via.placeholder.com/640x332.png" alt="Placeholder" />
+      </section>
+    </>
+  );
 }
