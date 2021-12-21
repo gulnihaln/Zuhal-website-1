37f66c525b42cde41da6e66ce4bff0155ba2e7c6
musayuksel <musayuxel@gmail.com>
2021-12-21 05:47:06 +0000

    feat:Add Lessons component fixes #5

--- zuhal-website/client/src/components/Lessons.js
+++ zuhal-website/client/src/components/Lessons.js
@@ -1,3 +1,22 @@
 export default function Lessons() {
-  return;
+  return (
+    <section>
+      <article>
+        <h1>
+          <span>We're</span> Coming Soon
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Voluptates modi, harum, omnis
+          reprehenderit a molestiae quidem exercitationem
+          qui ipsam quas nam quisquam.
+        </p>
+        <button>Contact With Us</button>
+      </article>
+      <img
+        src="https://via.placeholder.com/600x900.png"
+        alt=""
+      />
+    </section>
+  );
 }
