bbc073a86a61510bb9c98a9e370dedfaed1ff92b
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-12-22 16:08:45 +0000

    feat: add evening hair and makeup page fixes #28

--- zuhal-website/client/src/components/EveningHairAndMakeup.js
+++ zuhal-website/client/src/components/EveningHairAndMakeup.js
@@ -1,3 +1,22 @@
+import Slider from "./Slider"
+
 export default function EveningHairAndMakeup() {
-  return;
+  const images = [
+    { src: "https://via.placeholder.com/1440x400.png" },
+    { src: "https://via.placeholder.com/1400x400.png" },
+    { src: "https://via.placeholder.com/1410x400.png" }
+  ];
+
+  return (
+    <>
+      <Slider images={images} />
+      <section>
+        <img src="https://via.placeholder.com/650x350.png" alt=""></img>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, ratione totam laudantium ducimus quos praesentium veniam ipsam harum nam minima sit voluptatem nihil, doloremque id aperiam tempore quae quis consequatur iusto. Inventore aperiam, minima nobis quidem fugit odio delectus totam?
+        </p>
+      </section>
+      {/* <FAQ />  */}
+    </>
+  );
 }
