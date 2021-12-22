b639bdb5c0817b1bcb3746b7f6235b74b4495270
mohammed <mohammedalabdulhamid@gmail.com>
2021-12-22 01:33:06 +0000

    BrowsThreading removed from App

--- zuhal-website/client/src/App.js
+++ zuhal-website/client/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import BrowThreadingAndTinting from "./components/BrowThreadingAndTinting";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Zuhal's Make-up Website</header>
-      < BrowThreadingAndTinting />
+      
     </div>
   );
 }
