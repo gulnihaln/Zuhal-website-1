77a3dbdc8e755b6df3cd9bc0f5562b300e0b222e
Sharm-Dev <sharmainetaylor@ymail.com>
2021-12-22 10:46:24 +0000

    fixes #19, fixes #16

--- zuhal-website/client/src/components/TopNav.js
+++ zuhal-website/client/src/components/TopNav.js
@@ -1,3 +1,17 @@
-export default function TopNave() {
-  return;
+export default function TopNav() {
+  return (
+    <>
+      <section>
+        <img src="https://via.placeholder.com/24x24" alt="placeholder" />
+        <p>Phone Number</p>
+        <img src="https://via.placeholder.com/24x24" alt="placeholder" />
+        <p>Email</p>
+      </section>
+      <section>
+        <img src="https://via.placeholder.com/24x24" alt="placeholder" />
+        <img src="https://via.placeholder.com/24x24" alt="placeholder" />
+        <img src="https://via.placeholder.com/24x24" alt="placeholder" />
+      </section>
+    </>
+  );
 }
--- zuhal-website/client/src/components/Wedding.js
+++ zuhal-website/client/src/components/Wedding.js
@@ -1,3 +1,38 @@
 export default function Wedding() {
-  return;
+  return (
+    <>
+      <section>Carousel</section>
+      
+      <section>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut,
+          excepturi. Eveniet tempora neque ratione assumenda consequatur autem
+          sunt ipsam veniam!
+        </p>
+        <img src="https://via.placeholder.com/400x600" alt="placeholder" />
+      </section>
+      <section>
+        <img src="https://via.placeholder.com/400x600" alt="placeholder" />
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut,
+          excepturi. Eveniet tempora neque ratione assumenda consequatur autem
+          sunt ipsam veniam!
+        </p>
+      </section>
+      <section>
+        <h2>FAQ</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi sed
+          ipsam aspernatur minus eveniet, distinctio culpa pariatur quae
+          doloremque sapiente.
+        </p>
+        <ul>
+          <li>Lorem ipsum dolor sit amet consectetur</li>
+          <li>Lorem ipsum dolor sit amet consectetur</li>
+          <li>Lorem ipsum dolor sit amet consectetur</li>
+          <li>Lorem ipsum dolor sit amet consectetur</li>
+        </ul>
+      </section>
+    </>
+  );
 }
