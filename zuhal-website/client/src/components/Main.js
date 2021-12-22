f0b9ed6a389bf849ff252e79ad88d1cc89fff26d
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-12-22 15:25:31 +0000

    feat: add main component

--- zuhal-website/client/src/components/Main.js
+++ zuhal-website/client/src/components/Main.js
@@ -1,3 +1,34 @@
+
+import Slider from "./Slider";
+
 export default function Main() {
-  return;
+  return (
+    <>
+      <Slider />
+      <section className="section-intro">
+        <h2>Lorem ipsum dolor sit amet.</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quis
+          id consectetur, tempore iusto iure atque cumque ullam excepturi
+          dolores explicabo. Non error suscipit saepe dolor, labore deleniti nam
+          est?
+        </p>
+        <button>Lorem ipsum</button>
+        <img src="" alt=""></img>
+      </section>
+      <section className="sectionQuote">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+          aliquid.
+        </p>
+        <p className="quoteAuthor">Lorem ipsum dolor</p>
+      </section>
+      <section>
+        <img src="https://via.placeholder.com/650x350.png" alt=""></img>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, error.
+        </p>
+      </section>
+    </>
+  );
 }
