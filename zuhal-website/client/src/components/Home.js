d344811f5612b31014ce62cee0c4995801f61e13
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-12-22 15:29:28 +0000

    feat: add Home page fixes #7

--- zuhal-website/client/src/components/Home.js
+++ zuhal-website/client/src/components/Home.js
@@ -1,3 +1,14 @@
+import React from "react";
+import Header from "./Header"
+import Main from "./Main";
+import Footer from "./Footer";
+
 export default function Home() {
-  return;
+  return (
+    <>
+    <Header />
+    <Main />
+    <Footer />
+    </>
+  )
 }
