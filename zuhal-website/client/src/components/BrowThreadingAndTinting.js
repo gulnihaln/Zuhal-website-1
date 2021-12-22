a35a8374c193f5c9effacebc05e2ab1ddfb03dd0
mohammed <mohammedalabdulhamid@gmail.com>
2021-12-22 01:25:09 +0000

    BrowsThreading..

--- zuhal-website/client/src/components/BrowThreadingAndTinting.js
+++ zuhal-website/client/src/components/BrowThreadingAndTinting.js
@@ -1,3 +1,28 @@
 export default function BrowThreadingAndTinting() {
-  return;
+  return (
+    <>
+      <section className="section1-Brows">
+        <img
+          src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+          width="500"
+          height="250"
+          alt="section1-img"
+        />
+        <p id="section1-Brows">
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, quibusdam maxime. Recusandae reiciendis suscipit libero delectus qui animi dolore accusantium corporis non repellat optio rerum aut necessitatibus dolores vel eum tenetur aliquid, praesentium sunt cupiditate ullam nihil. Ea ex quis illo? Reiciendis dolorum delectus labore culpa nulla quisquam necessitatibus pariatur.
+        </p>
+      </section>
+      <section className="section2-Brows">
+        <p id="section2-Brows">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, vel! Id quibusdam iure dolorum eligendi deserunt consequuntur eius delectus iste, inventore quidem soluta hic repellat repellendus. Esse quisquam necessitatibus a cumque reprehenderit, minus nihil hic vitae? Nihil quidem quasi ad iste, magni velit omnis veritatis assumenda laborum, soluta aspernatur esse!
+        </p>
+        <img
+          src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+          width="500"
+          height="250"
+          alt="section2-img"
+        />
+      </section>
+    </>
+  );
 }
