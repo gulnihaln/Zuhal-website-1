1e568a29f03f1ef95d5ceb03b9bc8574b361386b
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-12-22 16:16:10 +0000

    fixes #7

--- zuhal-website/client/src/components/Home.js
+++ zuhal-website/client/src/components/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header"
 import Main from "./Main";
 import Footer from "./Footer";
+import EveningHairAndMakeup from "./EveningHairAndMakeup"
 
 export default function Home() {
   return (
     <>
     <Header />
     <Main />
+    {/* <EveningHairAndMakeup /> */}
     <Footer />
     </>
   )
