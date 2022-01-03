19eb0d89a4588acf8b0464efa72c73928e11b4e3
seyedsharifi12 <seyedsharifi12@gmail.com>
2022-01-03 19:46:44 +0000

    resolves #101

--- zuhal-website/client/src/components/Navigation.js
+++ zuhal-website/client/src/components/Navigation.js
@@ -8,7 +8,7 @@ import "./styles/Navigation.css";
 
 export default function Navigation() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [sidebar, setSidebar] = useState(true);
+  const [sidebar, setSidebar] = useState(false);
 
   const openDropdown = () => {
     setDropdownOpen(!dropdownOpen);
