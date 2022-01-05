d08c24070bfc1f271c95e20ce5bc29c74a3d8f83
Michael Shields <mr.m.p.shields+coding@gmail.com>
2022-01-05 20:33:00 +0000

    refactor: convert images as background images instead of <img> child elements

--- zuhal-website/client/src/components/PortfolioCategoryBttn.js
+++ zuhal-website/client/src/components/PortfolioCategoryBttn.js
@@ -1,24 +1,25 @@
 import imagesData from '../utils/imagesData'
 
 export default function PortfolioCategoryBttn({
-    setCategory, category 
+    setCategory, 
+    category 
 }) {
-    // Button image src set to the the first image in the images data
-    // that matches the category prop
-    // this logic will be replaced by a API request
 
     const imgData = imagesData.find(imgData => 
         imgData.category === category
     )
 
+    const styles = {
+        backgroundImage: `url(${imgData.src})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+    }
+
     return (
-        <button 
+        <button
+            style={styles}
             onClick={() => setCategory(imgData.category)}
             className="portfolio-category-bttn">
-            <img 
-                className="portfolio-category-img"
-                src={imgData.src}
-                alt={imgData.alt}/>
             <p className="portfolio-category-text">
                 {imgData.title}
             </p>
