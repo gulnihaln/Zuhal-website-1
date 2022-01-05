0602f05e3ab0a707b7d8e1f97fe82c587273fe25
seyedsharifi12 <seyedsharifi12@gmail.com>
2022-01-05 22:17:51 +0000

    -m all changes added'

--- zuhal-website/client/src/App.js
+++ zuhal-website/client/src/App.js
@@ -1,7 +1,12 @@
 import Home from "./components/Home";
 import "./App.css";
+import { IconContext } from "react-icons/lib";
 function App() {
-  return <Home />;
+  return (
+    <IconContext.Provider value={{ size: 24 }}>
+      <Home />
+    </IconContext.Provider>
+  );
 }
 
 export default App;
--- zuhal-website/client/src/components/Navigation.js
+++ zuhal-website/client/src/components/Navigation.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
 import { BsFillCaretDownFill } from "react-icons/bs";
 import { dropdown } from "../utils/DropdownData";
-import { MdContactMail } from "react-icons/md";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { GrGallery } from "react-icons/gr";
+
 import "./styles/Navigation.css";
 
 export default function Navigation() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [sidebar, setSidebar] = useState(false);
+  let { pathname } = useLocation();
+  console.log(pathname);
 
   const openDropdown = () => {
     setDropdownOpen(!dropdownOpen);
@@ -18,14 +19,24 @@ export default function Navigation() {
   const closeDropdown = () => {
     setDropdownOpen(false);
   };
+  const openSidebar = () => {
+    setSidebar(!sidebar);
+  };
 
   return (
     <>
       <div className="hamburger-menu">
-        <GiHamburgerMenu size={40} />
+        <GiHamburgerMenu
+          style={{ cursor: "pointer" }}
+          size={40}
+          onClick={() => {
+            openSidebar();
+            console.log(sidebar);
+          }}
+        />
       </div>
       <nav className="navbar">
-        <Link to="#">
+        <Link to="#" active>
           <img
             className="logo"
             src="/zuhal-cropped-logo-3-white-pink-16-3.png"
@@ -37,14 +48,8 @@ export default function Navigation() {
           Home
         </Link>
         <Link to="/aboutme">About Me</Link>
-        <Link to="/contact">
-          <MdContactMail style={{ margin: "0 5 3 0" }} />
-          Contact
-        </Link>
-        <Link to="/portfolio">
-          <GrGallery style={{ color: "white" }} />
-          Portfolio
-        </Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/portfolio">Portfolio</Link>
         <div className="dropdown" onMouseLeave={closeDropdown}>
           <button onClick={openDropdown} className="dropbtn">
             Services
