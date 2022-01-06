6b63648ef77737c208695756bd217d7fa34dd505
musayuksel <musayuxel@gmail.com>
2022-01-06 21:54:08 +0000

    Add new styles for slider fixes #127, fixes #120

--- zuhal-website/client/src/components/Slider.js
+++ zuhal-website/client/src/components/Slider.js
@@ -1,44 +1,26 @@
-import React, { useState, useEffect } from "react";
-import SlideRadioBtn from "./SlideRadioBtn";
+import ImageGallery from "react-image-gallery";
 import "./styles/Slider.css";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+
 export default function Slider({ images }) {
-  const [slideIndex, setSlideIndex] = useState(1);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      nextSlide();
-    }, 5000);
-    return () => clearTimeout(timeoutId);
+  const images2 = images.map((image) => {
+    return {
+      original: image.src || image.original,
+      originalAlt: image.alt || image.originalAlt,
+    };
   });
-
-  const nextSlide = () => {
-    if (slideIndex !== images.length - 1) {
-      setSlideIndex(slideIndex + 1);
-    } else if (slideIndex === images.length - 1) {
-      setSlideIndex(0);
-    }
-  };
-
   return (
     <section className="slider-container">
-      <div className="slider-img-container">
-        <TransitionGroup component={null}>
-          <CSSTransition key={slideIndex} timeout={2000} classNames={"slide"}>
-            <img
-              src={`${images[slideIndex].src}`}
-              alt={images[slideIndex].alt}
-            />
-          </CSSTransition>
-        </TransitionGroup>
-        <div className="slider-btn-container">
-          <SlideRadioBtn
-            setSlideIndex={setSlideIndex}
-            slideIndex={slideIndex}
-            images={images}
-          />
-        </div>
-      </div>
+      <ImageGallery
+        items={images2}
+        autoPlay={true}
+        showFullscreenButton={false}
+        showPlayButton={false}
+        showBullets={true}
+        slideDuration={2000}
+        swipingTransitionDuration={1000}
+        slideInterval={5000}
+        showThumbnails={false}
+      />
     </section>
   );
 }
