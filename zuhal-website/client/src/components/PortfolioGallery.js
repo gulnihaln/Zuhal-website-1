847f8e1b9703c49269a192712f542e0baa79b8f9
omerffaruk <farukyamak11044@gmail.com>
2022-03-04 12:24:06 +0000

    feat: import masonry and images data

--- zuhal-website/client/src/components/PortfolioGallery.js
+++ zuhal-website/client/src/components/PortfolioGallery.js
@@ -1,8 +1,11 @@
-export default function PortfolioGallery({category, setCategory}) {
-    return (
-        <div className="portfolio-gallery">
-            <button onClick={() => setCategory(null)}>x</button>
-            <p>{category} Gallery</p>
-        </div>
-    )
+import Masonry from "react-masonry-css";
+import imagesData from "../utils/imagesData";
+
+export default function PortfolioGallery({ category, setCategory }) {
+  return (
+    <div className="portfolio-gallery">
+      <button onClick={() => setCategory(null)}>x</button>
+      <p>{category} Gallery</p>
+    </div>
+  );
 }
