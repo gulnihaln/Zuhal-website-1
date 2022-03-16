36ed770a4a794fc2845a0f528bc9ca313b734673
omer <omer@idevu.io>
2022-03-16 21:26:33 +0000

    feat: create portfolio masonry

--- zuhal-website/client/src/components/PortfolioGallery.js
+++ zuhal-website/client/src/components/PortfolioGallery.js
@@ -1,11 +1,22 @@
-import Masonry from "react-masonry-css";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import imagesData from "../utils/imagesData";
 
 export default function PortfolioGallery({ category, setCategory }) {
+  const categoryImages = imagesData.filter((img) => img.category === category);
+  const columnsCountBreakPoints = { 350: 1, 750: 2, 1000: 3 };
   return (
     <div className="portfolio-gallery">
-      <button onClick={() => setCategory(null)}>x</button>
-      <p>{category} Gallery</p>
+      <header>
+        <p>{category} Gallery</p>
+        <button onClick={() => setCategory(null)}>x</button>
+      </header>
+      <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
+        <Masonry gutter={4}>
+          {categoryImages.map((image) => (
+            <img style={{ width: "100%" }} alt={image.alt} src={image.src} />
+          ))}
+        </Masonry>
+      </ResponsiveMasonry>
     </div>
   );
 }
