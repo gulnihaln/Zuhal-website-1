593a93b1a2927a6e52b5c71d68dc19c12ecbb513
omer <ffaruk.yamak@gmail.com>
2022-03-29 13:07:30 +0100

    feat: adjust background postiion, auto prettier changes

--- zuhal-website/client/src/components/PortfolioCategoryBttn.js
+++ zuhal-website/client/src/components/PortfolioCategoryBttn.js
@@ -1,28 +1,21 @@
-import imagesData from '../utils/imagesData'
+import imagesData from "../utils/imagesData";
 
-export default function PortfolioCategoryBttn({
-    setCategory, 
-    category 
-}) {
+export default function PortfolioCategoryBttn({ setCategory, category }) {
+  const imgData = imagesData.find((imgData) => imgData.category === category);
 
-    const imgData = imagesData.find(imgData => 
-        imgData.category === category
-    )
+  const styles = {
+    backgroundImage: `url(${imgData.src})`,
+    backgroundSize: "cover",
+    backgroundPosition: "50% 18%",
+  };
 
-    const styles = {
-        backgroundImage: `url(${imgData.src})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-    }
-
-    return (
-        <button
-            style={styles}
-            onClick={() => setCategory(imgData.category)}
-            className="portfolio-category-bttn">
-            <p className="portfolio-category-text">
-                {imgData.title}
-            </p>
-      </button>
-    )
+  return (
+    <button
+      style={styles}
+      onClick={() => setCategory(imgData.category)}
+      className="portfolio-category-bttn"
+    >
+      <p className="portfolio-category-text">{imgData.title}</p>
+    </button>
+  );
 }
