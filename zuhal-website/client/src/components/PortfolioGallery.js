386020d586b70bcabffa8190b5bcd573a32b0010
omer <ffaruk.yamak@gmail.com>
2022-03-29 13:06:33 +0100

    feat: scroll to the top when clicked on a portfolio category button

--- zuhal-website/client/src/components/PortfolioGallery.js
+++ zuhal-website/client/src/components/PortfolioGallery.js
@@ -1,12 +1,19 @@
+import { useEffect, useRef } from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import imagesData from "../utils/imagesData";
 
 export default function PortfolioGallery({ category, setCategory }) {
+  const ref = useRef();
   const categoryImages = imagesData.filter((img) => img.category === category);
   const columnsCountBreakPoints = { 350: 1, 750: 2, 1000: 3 };
+  useEffect(() => {
+    ref.current.scrollIntoView({
+      behavior: "smooth",
+    });
+  }, []);
   return (
     <div className="portfolio-gallery">
-      <header>
+      <header ref={ref}>
         <p>{category} Gallery</p>
         <button onClick={() => setCategory(null)}>x</button>
       </header>
