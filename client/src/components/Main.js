8a04c764223d6e63a0bd3f418359b7891e1596dc
musayuksel <musayuxel@gmail.com>
2022-05-09 19:54:40 +0100

    Feat:Add content and style tags

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import "./styles/PrimaryButton.css";
 import ImgTag from "./ImgTag";
 import HeroImage from "./HeroImage";
+import { Link } from "react-router-dom";
 
 export default function Main() {
 	useEffect(() => {
@@ -15,27 +16,53 @@ export default function Main() {
 				className="main-page-hero"
 			/>
 			<section className="section-intro">
-				<h2>Lorem ipsum dolor sit amet.</h2>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quis
-					id consectetur, tempore iusto iure atque cumque ullam excepturi
-					dolores explicabo. Non error suscipit saepe dolor, labore deleniti nam
-					est?
-				</p>
-				<button className="button-primary">Lorem ipsum</button>
-				<img src="" alt=""></img>
+				<article className="section-intro-article">
+					<h2>Hi, Welcome to Zuhal makeup artist.</h2>
+					<p>
+						My name is Zuhal and I create flawless makeup and hair styling with
+						elegance AND FLAIR I also love creating beauty and Editorial looks
+						based in Freiburg Germany. I have created hair and makeup styling
+						for weddings and special occasions clients. I am obsessed with the
+						wonderful and exciting world of beauty. I provide fresh, unique and
+						individual makeup and hairstyling services for weddings, special
+						occasions, editorials and fashion. I also offer eyebrow grooming and
+						tinting with different I am glad to receiving kind and wonderful
+						words from clients so I am confident that I am making them happy.
+						Please feel free to get in touch, I can’t wait to hear from you
+						whatever the occasion is.
+					</p>
+					<Link to={"/contact"}>
+						<button className="button-primary">Contact With Us</button>
+					</Link>
+				</article>
+				<ImgTag
+					category="bridal"
+					imgLink="bridal2.jpg"
+					alt="image1"
+					className="main-image-1"
+				/>
 			</section>
 			<section className="sectionQuote">
 				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
-					aliquid.
+					I believe all women are pretty without makeup, but with the right
+					makeup, they can be pretty adorable.
 				</p>
-				<p className="quoteAuthor">Lorem ipsum dolor</p>
+				<p className="quoteAuthor">-Zuhal Makeup</p>
 			</section>
-			<section>
-				<ImgTag category="specialOccasions" imgLink="image4.jpg" alt="image1" />
+			<section className="section-intro2">
+				<ImgTag
+					category="bridal"
+					imgLink="bridal4.jpg"
+					alt="image1"
+					className="main-image-2"
+				/>
 				<p>
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, error.
+					My work is described as Elegance with front row style with
+					sophistication. Every one of you have a unique style and the looks
+					that I create are never the same and always personalised. I know that
+					getting hair and makeup every time looking person is not easy thing to
+					do that’s why I am always hunting for new trends, skills and trying
+					new techniques.
 				</p>
 			</section>
 		</>
