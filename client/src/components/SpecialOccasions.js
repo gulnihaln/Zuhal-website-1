fb41e5741fc7f93bd2ebf8885110be830acd0f88
musayuksel <musayuxel@gmail.com>
2022-05-13 15:58:29 +0100

    Refactor:Add class names for style

--- client/src/components/SpecialOccasions.js
+++ client/src/components/SpecialOccasions.js
@@ -8,24 +8,29 @@ export default function SpecialOccasions() {
 			<picture className="d">
 				<source
 					media="(min-width:500px)"
-					srcSet={`${specialOccasions["image3.jpg"].default}`}
+					srcSet={`${specialOccasions["image7.jpg"].default}`}
 				/>
 				<img
 					className="hero-image special-occasions-hero"
-					src={`${specialOccasions["image5.jpg"].default}`}
+					src={`${specialOccasions["image11.jpeg"].default}`}
 					alt="Flowers"
 				/>
 			</picture>
 			<section className="occasion-middle-container">
-				<ImgTag category="specialOccasions" imgLink="image4.jpg" alt="image1" />
-
-				<p className="p-middle">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio,
-					ratione totam laudantium ducimus quos praesentium veniam ipsam harum
-					nam minima sit voluptatem nihil, doloremque id aperiam tempore quae
-					quis consequatur iusto. Inventore aperiam, minima nobis quidem fugit
-					odio delectus totam?
-				</p>
+				<ImgTag
+					category="specialOccasions"
+					imgLink="image8.jpg"
+					alt="image1"
+					className="special-occasion-image-1"
+				/>
+				<article className="special-occasion-intro-article">
+					<h2>Be Special...</h2>
+					<p className="p-middle">
+						If you have a special occasion, such as a wedding, festival or just
+						because you <span>DESERVE it </span> , book your special occasion
+						makeup to showcase your beauty in a unique way.
+					</p>
+				</article>
 			</section>
 			<section className="faq-section">
 				<article>
