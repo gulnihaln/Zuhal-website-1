4a0960ab538157a82d0bea4c07b8313d06478a8a
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-05-13 22:11:09 +0100

    about page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,7 +3,9 @@ const About = () => (
 		<div>
 			<h1>About</h1>
 			<p>
-				Basically we will be using this tenplate for deployment to Heroku. We can transfer our code from the other Github repo to here and hopefully it will work work. Thanks guys!
+				Basically we will be using this tenplate for deployment to Heroku. We
+				can transfer our code from the other Github repo to here and hopefully
+				it will work work. Thanks guys!
 			</p>
 		</div>
 	</main>
